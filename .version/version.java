f35d087b40ef2a0efe6c98db6e297db14fdeedce
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-01-21 02:53:52 -0600

    Update version.java

--- .version/version.java
+++ .version/version.java
@@ -5,5 +5,8 @@ class HelloWorld {
         System.out.println("Printing Log:");
         System.out.println("Now");
         System.out.println("Java Version 11 SE LTS JDK");
+        System.out.println("Log:");
+        System.out.println("No Critical Errors");
+        System.out.println("No Errors:)");
     }
 }
