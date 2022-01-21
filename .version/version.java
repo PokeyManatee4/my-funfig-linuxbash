7c6c3af17744c791ced78f05b50e668aa7b20d25
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-01-21 02:50:21 -0600

    Create version.java

---
+++ .version/version.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Java VM Starting");
+        System.out.println("VM Started!");
+        System.out.println("Printing Log:");
+        System.out.println("Now");
+        System.out.println("Java Version 11 SE LTS JDK");
+    }
+}
